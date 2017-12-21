d1bddac05ef7a5021abb7289c12a847b5c7da662
Bas Kardinaal <baswazz@gmail.com>
2017-12-21 20:53:43 +0100

    Added js stuff

--- js/app.js
+++ js/app.js
@@ -1,48 +1,19 @@
-/*
-mediabla.addEventListener('captionschange') => {
-
-}
-
-addEventListener('click', => {
-	
-})
-
-
+// Media element player settings
 $('video').mediaelementplayer({
+	features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen'], 
 	stretching: 'responsive',
-	startLanguage: 'en',
+});
 
-	success: function(media, node, intance) {
-		console.log('Media attached!');
-		media.addEventListener('captionschange', function () {
-			console.log('Listen for captionschange!');
-		});
+const video    = document.getElementsByTagName('video')[0];
+const captions = document.querySelectorAll('p.caption span');
+
+// Listen for time updates in video
+video.addEventListener('timeupdate', function() {
+
+	// console.log(video.currentTime);
+	
+	// Loop through the caption items
+	for (let i = 0; i < captions.length; i += 1) {
+		// console.log(captions[i]);
 	}
 });
-
-var videos = $('video').mediaelementplayer({
-	stretching: 'responsive',
-	startLanguage: 'en',	
-});
-
-Er is een div mejs__captions-text 6 hiermee kan je met een event laten listenen voor changes. Ook krijg je een getal mee.
-Deze kun je gebruiken om te zorgen dat ie het juiste tekstje bij de juiste caption toont.
-Waar je ook even naar moet kijken is dat hij de juiste div pakt want als er meer videos zijn...
-*/
-
-$('video').mediaelementplayer({
-	stretching: 'responsive',
-	startLanguage: 'en',
-});
-
-const captionChange = document.querySelector('span.mejs__captions-text');
-let test = captionChange.textContent;
-console.log(captionChange);
-test.addEventListener('change', () => {
-	console.log('Changed!');
-});
-
-
-// $(captionChange).bind("DOMSubtreeModified",function(){
-//   console.log('Changed!');
-// });
