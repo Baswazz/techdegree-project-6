cd2fe8543f3ac36148800d6850e15aa4464fbc9e
Bas Kardinaal <bkardinaal@trumanscout.com>
2017-12-22 09:17:20 +0100

    Added some Javascript

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $('video').mediaelementplayer({
 });
 
 const video    = document.getElementsByTagName('video')[0];
-const captions = document.querySelectorAll('p.caption span');
+const caption = document.querySelectorAll('p.caption span');
 
 // Listen for time updates in video
 video.addEventListener('timeupdate', function() {
@@ -13,7 +13,17 @@ video.addEventListener('timeupdate', function() {
 	// console.log(video.currentTime);
 	
 	// Loop through the caption items
-	for (let i = 0; i < captions.length; i += 1) {
+	for (let i = 0; i < caption.length; i += 1) {
 		// console.log(captions[i]);
+		let startTime = caption[i].dataset.start;
+		let endTime = caption[i].dataset.end;
+
+		if (video.currentTime > startTime && video.currentTime < endTime) {
+			// console.log('Data start: ', start);
+			// console.log('Data end: ', end);
+			caption[i].classList.add('highlight');
+		} else {
+			caption[i].classList.remove('highlight');
+		}
 	}
 });
