674087ae6e2b175969d3145d1af2c4d5e009ef8e
Bas Kardinaal <baswazz@gmail.com>
2017-12-23 18:04:07 +0100

    last changes

--- js/app.js
+++ js/app.js
@@ -4,26 +4,30 @@ $('video').mediaelementplayer({
 	stretching: 'responsive',
 });
 
-const video    = document.getElementsByTagName('video')[0];
+// Define variables
+const video   = document.getElementsByTagName('video')[0];
 const caption = document.querySelectorAll('p.caption span');
 
-// Listen for time updates in video
-video.addEventListener('timeupdate', function() {
-
-	// console.log(video.currentTime);
-	
+const captions = () => {
 	// Loop through the caption items
 	for (let i = 0; i < caption.length; i += 1) {
-		// console.log(captions[i]);
-		let startTime = caption[i].dataset.start;
-		let endTime = caption[i].dataset.end;
+		let startTime = parseFloat(caption[i].dataset.start);
+		let endTime = parseFloat(caption[i].dataset.end);
 
+		// If the start/end times match highlight caption
 		if (video.currentTime > startTime && video.currentTime < endTime) {
-			// console.log('Data start: ', start);
-			// console.log('Data end: ', end);
 			caption[i].classList.add('highlight');
 		} else {
 			caption[i].classList.remove('highlight');
 		}
+
+		// Listen for clicks and move to caption startTime
+		caption[i].addEventListener('click', () => {
+			video.currentTime = startTime;
+			video.play();
+		});
 	}
-});
+}
+
+// Listen for time updates in the video
+video.addEventListener('timeupdate', captions());
